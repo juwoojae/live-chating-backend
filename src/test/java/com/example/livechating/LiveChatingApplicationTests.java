dee33f741973fd22162ad6ed4919fdbbcecf5426
Juwoojae <juwqq1234@naver.com>
2025-11-28 20:46:42 +0900

    Initial commit

---
+++ src/test/java/com/example/livechating/LiveChatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.livechating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveChatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
